2dcc5a2dadd2a1f72139e2e522f2408f87397b5d
SerhiiQAA <nabukve@gmail.com>
2025-05-15 19:43:27 +0300

    base

--- cypress/e2e/tests/example.cy.ts
+++ cypress/e2e/tests/example.cy.ts
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-  })
-})
+import BasePage from '../pages/basePage';
+
+describe('Base Page Test', () => {
+  it('should visit the homepage', () => {
+    BasePage.visit(); 
+  });
+});
