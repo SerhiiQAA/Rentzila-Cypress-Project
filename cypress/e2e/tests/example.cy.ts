73ad350c71355fc0d2b8275a1e517bea114499d2
SerhiiQAA <nabukve@gmail.com>
2025-05-16 16:52:42 +0300

    test

--- cypress/e2e/tests/example.cy.ts
+++ cypress/e2e/tests/example.cy.ts
@@ -1,7 +1,10 @@
+import { config } from '../../../config';
 import BasePage from '../pages/BasePage';
+import Header from '../components/Header';
 
 describe('Base Page Test', () => {
   it('should visit the homepage', () => {
+    cy.viewport(config.viewportWidth, config.viewportHeight);
     BasePage.visit(); 
   });
 });
