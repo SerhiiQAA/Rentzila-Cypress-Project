98357e19467f8fecd13672862a9537af2ab39737
SerhiiQAA <nabukve@gmail.com>
2025-05-16 19:53:21 +0300

    viewport

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://dev.rentzila.com.ua/",
+    viewportWidth: 1280,  
+     
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
