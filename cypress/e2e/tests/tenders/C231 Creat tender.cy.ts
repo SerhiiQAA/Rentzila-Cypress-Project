c56e26beecd858300cfad4ddbb2f87129593d909
SerhiiQAA <nabukve@gmail.com>
2025-05-20 17:55:42 +0300

    test

--- cypress/e2e/tests/tenders/C231 Creat tender.cy.ts
+++ cypress/e2e/tests/tenders/C231 Creat tender.cy.ts
@@ -25,51 +25,51 @@ describe('Tenders functionality', () => {
     // 4.3 - Enter following invalid values into ""Назва тендера"" field
     TenderCreatePage.elements.tenderNameInput().clear().type('Tender name <')
     TenderCreatePage.elements.tenderNameInput()
-    .should('be.visible') 
-    .invoke('val')
-    .should('eq', 'Tender name ');
+      .should('be.visible')
+      .invoke('val')
+      .should('eq', 'Tender name ');
     // 4.4 Copy and paste the name with a restricted symbol.
     TenderCreatePage.elements.tenderNameInput().clear().type('Tender name >')
     TenderCreatePage.elements.tenderNameInput()
-    .should('be.visible') 
-    .invoke('val')
-    .should('eq', 'Tender name ');
+      .should('be.visible')
+      .invoke('val')
+      .should('eq', 'Tender name ');
     // 4.5 Enter the name that has 71 symbol
     TenderCreatePage.elements.tenderNameInput().clear().type('TendernameTendernameTendernameTendernameTendernameTendernameTendername1')
     TenderCreatePage.elements.tenderNameInput()
-    .should('be.visible') 
-    .invoke('val')
-    .should('eq', 'TendernameTendernameTendernameTendernameTendernameTendernameTendername');
+      .should('be.visible')
+      .invoke('val')
+      .should('eq', 'TendernameTendernameTendernameTendernameTendernameTendernameTendername');
     // 5. - Click the ""X"" icon near the service to clear the ""Послуга"" field
     TenderCreatePage.clickServiceListCloseBtn()
     TenderCreatePage.elements.findServiceInput()
-    .should('be.visible')
-    .invoke('val')
-    .should('eq', '');
+      .should('be.visible')
+      .invoke('val')
+      .should('eq', '');
     // 5.1 - Empty ""Послуга"" field - Click ""ДАЛІ""
     TenderCreatePage.clickNextBtn();
     TenderCreatePage.elements.findServiceInputErrorMsg().scrollIntoView().should('have.text', 'Це поле обов’язкове')
     // 5.2 Enter a valid value with the restricted symbol at the end
     TenderCreatePage.elements.findServiceInput().type('Перевезення урожаю <')
     TenderCreatePage.elements.findServiceInput()
-    .should('be.visible')
-    .invoke('val')
-    .should('eq', 'Перевезення урожаю ');
+      .should('be.visible')
+      .invoke('val')
+      .should('eq', 'Перевезення урожаю ');
     // 5.3 Enter the value that has 101 symbol.
     TenderCreatePage.elements.findServiceInput().type('Перевезення урожаюююПеревезення урожаюююПеревезення урожаюююПеревезення урожаюююПеревезення урожаююю1')
     TenderCreatePage.elements.findServiceModalErrorMsg()
-    .should('be.visible')
-    .invoke('text')
-    .then((text) => {
-        cy.log(`Actual text received: ${text}`); 
-        const normalizedText = text.replace(/\s+/g, ' ').replace(/&nbsp;/g, ' ').trim(); 
+      .should('be.visible')
+      .invoke('text')
+      .then((text) => {
+        cy.log(`Actual text received: ${text}`);
+        const normalizedText = text.replace(/\s+/g, ' ').replace(/&nbsp;/g, ' ').trim();
         expect(normalizedText).to.include('На жаль, послугу “Перевезення урожаю Перевезення урожаюююПеревезення урожаюююПеревезення урожаюююПеревезення урожаюююП“ не знайдено в нашій базі.');
-    });
+      });
     // 5.4 Empty the field again. Copy and paste the value with a restricted symbol.
     TenderCreatePage.elements.findServiceInput().clear().type('Перевезення урожаю >')
     TenderCreatePage.elements.findServiceInput()
-    .should('be.visible')
-    .invoke('val')
-    .should('eq', 'Перевезення урожаю ');
+      .should('be.visible')
+      .invoke('val')
+      .should('eq', 'Перевезення урожаю ');
   });
 });
